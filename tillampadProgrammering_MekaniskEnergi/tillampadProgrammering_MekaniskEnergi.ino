6bc1c7cdcd120ba82beaeb924cddf68dc32d77c0
BotDot <alex.v.stupak@gmail.com>
2025-12-01 10:33:16 +0100

    Update tillampadProgrammering_MekaniskEnergi.ino

--- tillampadProgrammering_MekaniskEnergi/tillampadProgrammering_MekaniskEnergi.ino
+++ tillampadProgrammering_MekaniskEnergi/tillampadProgrammering_MekaniskEnergi.ino
@@ -17,56 +17,95 @@
 */
 //Libraries:
 #include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
-#include "SparkFunBME280.h"
-//Objects:
 
-BME280 mySensorA; //Uses default I2C address 0x77
-BME280 mySensorB; //Uses I2C address 0x76 (jumper closed)
 
-//Public constants:
-int MpPa=12;
-float StartPressure = mySensorA.readFloatPressure();
-float Height;
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("Example showing alternate I2C addresses");
+Adafruit_BMP280 bmp; // I2C
 
-  Wire.begin();
+//constant variables:
+const int button_pin= 8;
+const int ArraySize = 10;
+int filterArray[ArraySize];
 
-  mySensorA.setI2CAddress(0x77); //The default for the SparkFun Environmental Combo board is 0x77 (jumper open).
-  //If you close the jumper it is 0x76
-  //The I2C address must be set before .begin() otherwise the cal values will fail to load.
+//temp variables/dynamic variables:
+float start_height;
+float current_height;
+float heightPos;
+int index = 0;
+int i = 0;
 
-  if(mySensorA.beginI2C() == false) Serial.println("Sensor A connect failed");
 
-  mySensorB.setI2CAddress(0x76); //Connect to a second sensor
-  if(mySensorB.beginI2C() == false) Serial.println("Sensor B connect failed");
+//setup
+void setup() {
+  Serial.begin(9600);
+  pinMode(OUTPUT, button_pin);
+  while ( !Serial ) delay(100);   // wait for native usb
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  }
+
+
+ 
+void loop() {
+  current_height = bmp.readAltitude(1013.25);
+  if(digitalRead(button_pin) == LOW)
+   { start_height = bmp.readAltitude(1013.25);
+  }
+  heightPos = (current_height - start_height)* 100;
+  
+    /*
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); 
+    Serial.println(" m");
+
+    Serial.println();
+    delay(2000);
+    
+    */
+    Serial.print("Height:");
+    Serial.print(heightPos);
+    Serial.println(" cm");
+    filter(heightPos);
+    delay(1000);
 }
 
-void loop()
-{
-  Height = mySensorA.readFloatPressure() - StartPressure;
-  Serial.println(Height/MpPa);
 
-  Serial.print(" PressureA: ");
-  Serial.print(mySensorA.readFloatPressure(), 0);
 
-  Serial.print(" TempA: ");
-  Serial.print(mySensorA.readTempC(), 2);
- /*
-  Serial.print(" HumidityB: ");
-  Serial.print(mySensorB.readFloatHumidity(), 0);
-  
-  Serial.print(" PressureB: ");
-  Serial.print(mySensorB.readFloatPressure(), 0);
-  
-  Serial.print(" TempB: ");
-  Serial.print(mySensorB.readTempC(), 2);
+// filter function
+// utilizes arrays to give a rounded value that has been read. ||
+// Parameters: nothing ||
+// Output: float ||
+void filter(float input) {
+  for(int i = 0; i < ArraySize; i++){
+  filterArray[index] = input;
+  index = (index + 1) % 10;
+  }
+  float sum = 0.0;
+  float avg = 0.0;
 
-  Serial.println();
-*/
+  for (int i = 0; i < 10; i++) {
+    sum += filterArray[i];
+  }
+  avg = sum / 10.0;
+  avg = avg + 0;
+  Serial.println(avg);
   delay(50);
+
+
 }
+
+
+
+
 
