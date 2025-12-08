6f71daa80432eb209245991e296e060f21eda742
BotDot <alex.v.stupak@gmail.com>
2025-12-08 10:56:04 +0100

    Fixade med accelerometer

--- tillampadProgrammering_MekaniskEnergi/tillampadProgrammering_MekaniskEnergi.ino
+++ tillampadProgrammering_MekaniskEnergi/tillampadProgrammering_MekaniskEnergi.ino
@@ -19,19 +19,24 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
-
-
-Adafruit_BMP280 bmp; // I2C
+//Class objects:
+Adafruit_BMP280 bmp; 
+Adafruit_MPU6050 mpu;
 
 //constant variables:
 const int button_pin= 8;
 const int ArraySize = 10;
 int filterArray[ArraySize];
-
+const float weight = 0.1 //Weight in kg
+const float gravconst = 9.82 //the gravitational constant approx. 9.82 
 //temp variables/dynamic variables:
 float start_height;
 float current_height;
+float potentialenergy;
 float heightPos;
 int index = 0;
 int i = 0;
@@ -39,12 +44,16 @@ int i = 0;
 
 //setup
 void setup() {
-  Serial.begin(9600);
+  mpu.begin();
+  Serial.begin(115200);
   pinMode(OUTPUT, button_pin);
   while ( !Serial ) delay(100);   // wait for native usb
   Serial.println(F("BMP280 test"));
   unsigned status;
   status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   }
 
 
@@ -54,8 +63,10 @@ void loop() {
   if(digitalRead(button_pin) == LOW)
    { start_height = bmp.readAltitude(1013.25);
   }
-  heightPos = (current_height - start_height)* 100;
   
+  heightPos = (current_height - start_height) * 100;
+  potentialenergy = weight * heightPos * gravconst
+
     /*
     Serial.print(F("Temperature = "));
     Serial.print(bmp.readTemperature());
@@ -73,11 +84,34 @@ void loop() {
     delay(2000);
     
     */
+    
     Serial.print("Height:");
-    Serial.print(heightPos);
+    Serial.print(filter(heightPos));
     Serial.println(" cm");
     filter(heightPos);
+
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
     delay(1000);
+    Serial.print("AccelX:");
+    Serial.print(a.acceleration.x);
+    Serial.print(",");
+    Serial.print("AccelY:");
+    Serial.print(a.acceleration.y);
+    Serial.print(",");
+    Serial.print("AccelZ:");
+    Serial.print(a.acceleration.z);
+    Serial.print(", ");
+    Serial.print("GyroX:");
+    Serial.print(g.gyro.x);
+    Serial.print(",");
+    Serial.print("GyroY:");
+    Serial.print(g.gyro.y);
+    Serial.print(",");
+    Serial.print("GyroZ:");
+    Serial.print(g.gyro.z);
+    Serial.println("");
 }
 
 
@@ -86,11 +120,10 @@ void loop() {
 // utilizes arrays to give a rounded value that has been read. ||
 // Parameters: nothing ||
 // Output: float ||
-void filter(float input) {
-  for(int i = 0; i < ArraySize; i++){
+float filter(float input) {
   filterArray[index] = input;
-  index = (index + 1) % 10;
-  }
+  index= (index + 1) % 10;
+  
   float sum = 0.0;
   float avg = 0.0;
 
@@ -98,8 +131,7 @@ void filter(float input) {
     sum += filterArray[i];
   }
   avg = sum / 10.0;
-  avg = avg + 0;
-  Serial.println(avg);
+  return avg;
   delay(50);
 
 
